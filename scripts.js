4eb84b6ab5b59b83fdd5e0fce9fbb6c4643f3531
jeb192004 <jeb192004@Gmail.com>
2017-05-18 07:41:26 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -30,9 +30,9 @@ function contactHtmlFromObject(toons){
       
       html += '<p><div>'
 	 	   + '<img id= "img" src="'+toons.photoUrl+'"alt="enter"/>'
-	  	   + '</div>'+'</p>;
-	html += '<p>'+toons.ctoons.catagory+'</p>;
-                html += '<p>"added by: " + toons.userId
+	  	   + '</div>'+'</p>';
+	html += '<p>'+toons.catagory+'</p>;
+                html += '<p>"added by: " +toons.userId
                 +'</p>';
     html += '</div>';
   html += '</li>';
