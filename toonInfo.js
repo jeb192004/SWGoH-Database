546588b1e9e7948cd3da1cb37586b76b531dfcf5
jeb192004 <jeb192004@gmail.com>
2017-05-19 18:02:43 -0400

    added power/speed/health

--- toonInfo.js
+++ toonInfo.js
@@ -1,18 +1,20 @@
 //create firebase reference
 var dbRef = new Firebase("https://swgoh-campanion.firebaseio.com/");
-var contactsRef = dbRef.child('toons');
+var contactsRef = dbRef.child('Bistan');
 var toon = sessionStorage.getItem('toon');
 
 document.getElementById("header").innerHTML = sessionStorage.getItem('toon');
 window.sessionStorage.removeItem('toon');
 
 //load older conatcts as well as any newly added one...
-contactsRef.on("child_added", function(snap) {
+contactsRef.on("value", function(snap) {
   console.log("added", snap.key(), snap.val());
   document.querySelector('#toons')
     .innerHTML += contactHtmlFromObject(snap.val());
 	
-      
+     
+    
+
 // For each <li> inside #links
 for (var i = 0; i < links.length; i++) {
   var link = links[i];
@@ -23,17 +25,12 @@ for (var i = 0; i < links.length; i++) {
 //prepare conatct object's HTML
 function contactHtmlFromObject(toons){
   console.log( toons );
-  var html = '';
-  html += '<li class="list-group-item contact">';
-    html += '<div>';
-      html += '<p class="lead">'+toons.name+'</p>';
-      html += '<p>'+toons.catagory+'</p>';
-      html += '<p><small title="'
-                +toons.photoUrl+'">'
-                +toons.userId
-                +'</small></p>';
-    html += '</div>';
-  html += '</li>';
+  
+  var y = document.getElementById("power").innerHTML = toons.power;
+  var y = document.getElementById("speed").innerHTML = toons.speed;
+  var y = document.getElementById("health").innerHTML = toons.health;
+  
+  
   
   return html;
 }
