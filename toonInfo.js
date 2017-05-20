118aaadf26fe49bfd6cb39254e5d72a52542545b
jeb192004 <jeb192004@gmail.com>
2017-05-20 08:58:56 -0400

    home button and special ability

--- toonInfo.js
+++ toonInfo.js
@@ -1,11 +1,11 @@
 //create firebase reference
 var dbRef = new Firebase("https://swgoh-campanion.firebaseio.com/");
 var toon = sessionStorage.getItem('toon');
-var contactsRef = dbRef.child(toon);
+var contactsRef = dbRef.child('Bistan');
 
 
 document.getElementById("header").innerHTML = sessionStorage.getItem('toon');
-window.sessionStorage.removeItem('toon');
+
 
 //load older conatcts as well as any newly added one...
 contactsRef.on("value", function(snap) {
@@ -23,6 +23,11 @@ for (var i = 0; i < links.length; i++) {
 }
 });
 
+$('#home').click(function(){
+    window.location = 'index.html';
+	window.sessionStorage.removeItem('toon');
+});
+
 //prepare conatct object's HTML
 function contactHtmlFromObject(toons){
   console.log( toons );
@@ -38,8 +43,15 @@ function contactHtmlFromObject(toons){
   var y = document.getElementById("basicImg").innerHTML = '<img src="' + toons.basic_url + '"/>';
   var y = document.getElementById("basicDescription").innerHTML = toons.basic_description;
   var y = document.getElementById("basicDmg").innerHTML = "Damage: " + toons.basic_damage;
-  var y = document.getElementById("basicAbilImg").innerHTML = toons.basic_ability_image;
-  
+  var y = document.getElementById("basicAbilImg").innerHTML = '<img src="'+toons.basic_ability_image+'"/>';
+  //special 1 ability
+  var y = document.getElementById("sp1Level").innerHTML = "Basic: level " + toons.special_level;
+  var y = document.getElementById("sp1Name").innerHTML = toons.special_name;
+  var y = document.getElementById("sp1Img").innerHTML = '<img src="' + toons.special_url + '"/>';
+  var y = document.getElementById("sp1Description").innerHTML = toons.special_description;
+  var y = document.getElementById("sp1Dmg").innerHTML = toons.cooldown_special+" turn cooldown" +'<br>'+
+  "Damage: " + toons.special_damage;
+  var y = document.getElementById("sp1AbilImg").innerHTML = '<img src="'+toons.special_ability_image+'"/>';
   
   
   
