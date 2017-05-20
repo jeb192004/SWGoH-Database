abac6c4d8b5c83ddc877cad0c550ee537945b3d6
jeb192004 <jeb192004@gmail.com>
2017-05-20 08:05:53 -0400

    added basic attack info

--- toonInfo.js
+++ toonInfo.js
@@ -27,11 +27,19 @@ for (var i = 0; i < links.length; i++) {
 function contactHtmlFromObject(toons){
   console.log( toons );
   
-  
+  //description-power-speed-health
   var y = document.getElementById("toon-description").innerHTML = toons.toon_description;
   var y = document.getElementById("power").innerHTML = toons.power;
   var y = document.getElementById("speed").innerHTML = toons.speed;
   var y = document.getElementById("health").innerHTML = toons.health;
+  //basic ability
+  var y = document.getElementById("basicLevel").innerHTML = "Basic: level " + toons.basic_level;
+  var y = document.getElementById("basicName").innerHTML = toons.basic_name;
+  var y = document.getElementById("basicImg").innerHTML = '<img src="' + toons.basic_url + '"/>';
+  var y = document.getElementById("basicDescription").innerHTML = toons.basic_description;
+  var y = document.getElementById("basicDmg").innerHTML = "Damage: " + toons.basic_damage;
+  var y = document.getElementById("basicAbilImg").innerHTML = toons.basic_ability_image;
+  
   
   
   
