6ca8649fb24edbca8111bddc4ece6b1f2976fa89
jeb192004 <jeb192004@gmail.com>
2017-05-25 20:22:07 -0400

    y

--- js/editToonAbil.js
+++ js/editToonAbil.js
@@ -1,5 +1,14 @@
 
-
+var provider = new firebase.auth.FacebookAuthProvider();
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user;
+});
 
 
 var dbRef = new Firebase('https://swgoh-campanion.firebaseio.com/');
