798d52e9520b55605db8d58eff517a8bc488420a
jeb192004 <jeb192004@gmail.com>
2017-05-30 13:38:58 -0400

    redirect

--- js/logIn.js
+++ js/logIn.js
@@ -1,5 +1,5 @@
 var redirect = sessionStorage.getItem('redirect');
-
+window.sessionStorage.setItem('redirect',redirect);
 var config = {
     apiKey: "AIzaSyCG184jd5tjKzBDRRXYVmIm53o_n33g04E",
     authDomain: "swgoh-campanion.firebaseapp.com",
@@ -11,7 +11,26 @@ var config = {
   firebase.initializeApp(config);
   
   
-  
+  // FirebaseUI config.
+      var uiConfig = {
+	  
+        signInSuccessUrl: 'displayName.html',
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+          //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: '<your-tos-url>'
+      };
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
   
   
   
