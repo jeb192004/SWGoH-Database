3f5539e79a24f18c38482713fc15d7b7d6570b74
jeb192004 <jeb192004@gmail.com>
2017-05-30 12:41:49 -0400

    edit toon

--- js/editToonAbil.js
+++ js/editToonAbil.js
@@ -36,6 +36,25 @@ document.getElementById("u2btn").onclick = function () {
     messagingSenderId: "298882100900"
   };
   firebase.initializeApp(config);
+  initApp = function() {
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            
+            user.getToken().then(function(accessToken) {
+              document.getElementById("sign-out").style.display="block";
+            });
+          } else {
+         document.getElementById("sign-out").style.display="none";
+          }
+        }, function(error) {
+          console.log(error);
+        });
+      };
+      window.addEventListener('load', function() {
+        initApp();
+      });
   
       
 
