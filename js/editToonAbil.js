41ca6c69b1fc86aa243246f21a84ebf7dbdefbe4
jeb192004 <jeb192004@gmail.com>
2017-06-05 09:15:28 -0400

    edit

--- js/editToonAbil.js
+++ js/editToonAbil.js
@@ -13,12 +13,12 @@ contactsRef.once("value", function(snap) {
   document.getElementById('power').value = toons.power;
   document.getElementById("speed").value = toons.speed;
   document.getElementById("health").value = toons.health;
-  /**
+ 
   document.getElementById('bname').value = toons.basic_name;
   document.getElementById('bdescription').value = basic_description;
   document.getElementById('blevel').value = basic_level;
   document.getElementById('bdamage').value = basic_damage;
-	
+	/**
 	var sname1 = document.getElementById('sname1');
 	if (sname1 && sname1.value) {
   	sname1_1 = sname1.value;}
@@ -154,108 +154,171 @@ document.getElementById("u2btn").onclick = function () {
 	
 	var description = document.getElementById('description');
 	if (description && description.value) {
-  	var description_1 = description.value;}
+  	var description_1 = description.value;}else{
+		description_1 = "0";
+	}
 	var power = document.getElementById('power');
 	if (power && power.value) {
-  	var power_1 = power.value;}
+  	var power_1 = power.value;}else{
+		power_1 = "0";
+	}
 	var speed = document.getElementById('speed');
 	if (speed && speed.value) {
-  	var speed_1 = speed.value;}
-	
+  	var speed_1 = speed.value;}else{
+		speed_1 = "0";
+	}
 	var health = document.getElementById('health');
 	if (health && health.value) {
-  	var health_1 = health.value;}
+  	var health_1 = health.value;}else{
+		health_1 = "0";
+	}
 
 	var bname = document.getElementById('bname');
 	if (bname && bname.value) {
-  	var bname_1 = bname.value;}
+  	var bname_1 = bname.value;}else{
+		bname_1 = "0";
+	}
 	var bdescription = document.getElementById('bdescription');
 	if (bdescription && bdescription.value) {
-  	bdescription_1 = bdescription.value;}
+  	var bdescription_1 = bdescription.value;}else{
+		bdescription_1 = "0";
+	}
 	var blevel = document.getElementById('blevel');
 	if (blevel && blevel.value) {
-  	blevel_1 = blevel.value;}
+  	var blevel_1 = blevel.value;}else{
+		blevel_1 = "0";
+	}
 	var bdamage = document.getElementById('bdamage');
 	if (bdamage && bdamage.value) {
-  	bdamage_1 = bdamage.value;}
+  	var bdamage_1 = bdamage.value;}else{
+		bdamage_1 = "0";
+	}
 	
 	var sname1 = document.getElementById('sname1');
 	if (sname1 && sname1.value) {
-  	sname1_1 = sname1.value;}
+  	var sname1_1 = sname1.value;}else{
+		sname1_1 = "0";
+	}
 	var sdescription1 = document.getElementById('sdescription1');
 	if (sdescription1 && sdescription1.value) {
-  	sdescription1_1 = sdescription1.value;}
+  	var sdescription1_1 = sdescription1.value;}else{
+		sdescription1_1 = "0";
+	}
 	var slevel1 = document.getElementById('slevel1');
 	if (slevel1 && slevel1.value) {
-  	slevel1_1 = slevel1.value;}
+  	var slevel1_1 = slevel1.value;}else{
+		slevel1_1 = "0";
+	}
 	var sdamage1 = document.getElementById('sdamage1');
 	if (sdamage1 && sdamage1.value) {
-  	sdamage1_1 = sdamage1.value;}
+  	var sdamage1_1 = sdamage1.value;}else{
+		sdamage1_1 = "0";
+	}
 	var scooldown1 = document.getElementById('scooldown1');
 	if (scooldown1 && scooldown1.value) {
-  	scooldown1_1 = scooldown1.value;}
+  	var scooldown1_1 = scooldown1.value;}else{
+		scooldown1_1 = "0";
+	}
 	
 	var sname2 = document.getElementById('sname2');
 	if (sname2 && sname2.value) {
-  	sname2_1 = sname2.value;}
+  	var sname2_1 = sname2.value;}else{
+		sname2_1 = "0";
+	}
 	var sdescription2 = document.getElementById('sdescription2');
 	if (sdescription2 && sdescription2.value) {
-  	sdescription2_1 = sdescription2.value;}
+  	var sdescription2_1 = sdescription2.value;}else{
+		sdescription2_1 = "0";
+	}
 	var slevel2 = document.getElementById('slevel2');
 	if (slevel2 && slevel2.value) {
-  	slevel2_1 = slevel2.value;}
+  	var slevel2_1 = slevel2.value;}else{
+		slevel2_1 = "0";
+	}
 	var sdamage2 = document.getElementById('sdamage2');
 	if (sdamage2 && sdamage2.value) {
-  	sdamage2_1 = sdamage2.value;}
+  	var sdamage2_1 = sdamage2.value;}else{
+		sdamage2_1 = "0";
+	}
 	var scooldown2 = document.getElementById('scooldown2');
 	if (scooldown2 && scooldown2.value) {
-  	scooldown2_1 = scooldown2.value;}
+  	var scooldown2_1 = scooldown2.value;}else{
+		scooldown2_1 = "0";
+	}
 	
 	var sname3 = document.getElementById('sname3');
 	if (sname3 && sname3.value) {
-  	sname3_1 = sname3.value;}
+  	var sname3_1 = sname3.value;}else{
+		sname3_1 = "0";
+	}
 	var sdescription3 = document.getElementById('sdescription3');
 	if (sdescription3 && sdescription3.value) {
-  	sdescription3_1 = sdescription3.value;}
+  	var sdescription3_1 = sdescription3.value;}else{
+		sdescription3_1 = "0";
+	}
 	var slevel3 = document.getElementById('slevel3');
 	if (slevel3 && slevel3.value) {
-  	slevel3_1 = slevel3.value;}
+  	var slevel3_1 = slevel3.value;}else{
+		slevel3_1 = "0";
+	}
 	var sdamage3 = document.getElementById('sdamage3');
 	if (sdamage3 && sdamage3.value) {
-  	sdamage3_1 = sdamage3.value;}
+  	var sdamage3_1 = sdamage3.value;}else{
+		sdamage3_1 = "0";
+	}
 	var scooldown3 = document.getElementById('scooldown3');
 	if (scooldown3 && scooldown3.value) {
-  	scooldown3_1 = scooldown3.value;}
+  	var scooldown3_1 = scooldown3.value;}else{
+		scooldown3_1 = "0";
+	}
 	
 	var lname = document.getElementById('lname');
 	if (lname && lname.value) {
-  	lname_1 = lname.value;}
+  	var lname_1 = lname.value;}else{
+		lname_1 = "0";
+	}
 	var ldescription = document.getElementById('ldescription');
 	if (ldescription && ldescription.value) {
-  	ldescription_1 = ldescription.value;}
+  	var ldescription_1 = ldescription.value;}else{
+		ldescription_1 = "0";
+	}
 	var llevel = document.getElementById('llevel');
 	if (llevel && llevel.value) {
-  	llevel_1 = llevel.value;}
+  	var llevel_1 = llevel.value;}else{
+		llevel_1 = "0";
+	}
 	
 	var uname1 = document.getElementById('uname1');
 	if (uname1 && uname1.value) {
-  	uname1_1 = uname1.value;}
+  	var uname1_1 = uname1.value;}else{
+		uname1_1 = "0";
+	}
 	var udescription1 = document.getElementById('udescription1');
 	if (udescription1 && udescription1.value) {
-  	udescription1_1 = udescription1.value;}
+  	var udescription1_1 = udescription1.value;}else{
+		udescription1_1 = "0";
+	}
 	var ulevel1 = document.getElementById('ulevel1');
 	if (ulevel1 && ulevel1.value) {
-  	ulevel1_1 = ulevel1.value;}
+  	var ulevel1_1 = ulevel1.value;}else{
+		ulevel1_1 = "0";
+	}
 	
 	var uname2 = document.getElementById('uname2');
 	if (uname2 && uname2.value) {
-  	uname2_1 = uname2.value;}
+  	var uname2_1 = uname2.value;}else{
+		uname2_1 = "0";
+	}
 	var udescription2 = document.getElementById('udescription2');
 	if (udescription2 && udescription2.value) {
-  	udescription2_1 = udescription2.value;}
+  	var udescription2_1 = udescription2.value;}else{
+		udescription2_1 = "0";
+	}
 	var ulevel2 = document.getElementById('ulevel2');
 	if (ulevel2 && ulevel2.value) {
-  	ulevel2_1 = ulevel2.value;}
+  	var ulevel2_1 = ulevel2.value;}else{
+		ulevel2_1 = "0";
+	}
 
 
 	
@@ -266,7 +329,7 @@ toonAbil.set({
   	power: power_1,
   	speed: speed_1,
 	health: health_1,
-	/**basic_name: bname_1,
+	basic_name: bname_1,
   	basic_description: bdescription_1,
   	basic_level: blevel_1,
 	basic_damage: bdamage_1,
@@ -301,7 +364,7 @@ toonAbil.set({
 	leader_ability_image: lAbilImg_1,
 	unique1_ability_image: uAbilImg1_1,
 	unique2_ability_image: uAbilImg2_1,
-	**/
+
 });
 //document.getElementById('bname').value = '';
             });
