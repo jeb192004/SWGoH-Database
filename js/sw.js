2929a6b812d80142925f7f97d8c79c1afea461cc
jeb192004 <jeb192004@gmail.com>
2018-08-06 13:55:09 -0400

    sw

--- js/sw.js
+++ js/sw.js
@@ -2,7 +2,6 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('swgoh.co').then(function(cache) {
      return cache.addAll([
-        '/',
   		'/css/styles.css',
  		'/js/scripts.js'
      ]);
