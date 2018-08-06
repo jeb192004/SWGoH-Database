f39f392d62bd27deab62adde15066240a992147b
jeb192004 <jeb192004@gmail.com>
2018-08-06 12:59:42 -0400

    sw.js

--- js/scripts.js
+++ js/scripts.js
@@ -270,6 +270,6 @@ function shard_loc_item(shard_loc){
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('/pwa-examples/a2hs/sw.js')
+           .register('/js/sw.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
