027cfbba3dcad8b71ec7218eb81f112131ea7ab5
jeb192004 <jeb192004@gmail.com>
2018-08-07 07:25:03 -0400

    sw

---
+++ js/sw.js
@@ -0,0 +1,28 @@
+alert('sw registered');
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('swgoh.co').then(function(cache) {
+     return cache.addAll([
+	 	'/',
+	 	'/index.html',
+		'/js/scripts.js',
+  		'/css/styles.css'
+ 		
+		
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+
+
